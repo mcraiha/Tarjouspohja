bc8e8ea33cef2cc28b09bb8ddf858c31188a6ca7
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-07-17 20:45:36 +0300

    Service worker nosto
    Uusia ominaisuuksia

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 // Service worker
 
 // Välimuistin nimi, versionumeroa kasvatetaan päivityksien yhteydessä
-const PRECACHE = 'v3';
+const PRECACHE = 'v4';
 
 // Lista URL:eista, jotka otetaan aina alussa välimuistiin
 const PRECACHE_URLS = [
