a41b1867231a90d4f90ae12d6ca64302d382c401
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-08-01 15:31:41 +0300

    PRECACHE nosto
    Nyt v5

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 // Service worker
 
 // Välimuistin nimi, versionumeroa kasvatetaan päivityksien yhteydessä
-const PRECACHE = 'v4';
+const PRECACHE = 'v5';
 
 // Lista URL:eista, jotka otetaan aina alussa välimuistiin
 const PRECACHE_URLS = [
