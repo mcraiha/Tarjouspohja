22f8c62ca7bd5c31704cf481735b34ae868a2e96
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-01-30 11:42:36 +0200

    Versionumeron kasvatus
    Niin ei tartte force-ladata uudelleen

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 // Service worker
 
 // Välimuistin nimi, versionumeroa kasvatetaan päivityksien yhteydessä
-const PRECACHE = 'v6';
+const PRECACHE = 'v7';
 
 // Lista URL:eista, jotka otetaan aina alussa välimuistiin
 const PRECACHE_URLS = [
