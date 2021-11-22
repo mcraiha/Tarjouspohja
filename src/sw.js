fdb9698f524c7192b6a43af38d08e7644e20ec14
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2021-11-22 12:36:53 +0200

    Version nosto
    Jotta service worker on ajan tasalla

--- src/sw.js
+++ src/sw.js
@@ -1,7 +1,7 @@
 // Service worker
 
 // Välimuistin nimi, versionumeroa kasvatetaan päivityksien yhteydessä
-const PRECACHE = 'v7';
+const PRECACHE = 'v8';
 
 // Lista URL:eista, jotka otetaan aina alussa välimuistiin
 const PRECACHE_URLS = [
